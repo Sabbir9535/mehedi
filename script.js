a60240a65fdbd82a3555fabb1167e8e0d6cb6ed7
Sabbir hossen <74649145+Sabbir9535@users.noreply.github.com>
2024-10-15 21:39:13 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById("yes").addEventListener("click",function(){
+document.getElementById("display").innerText="I love you Fariha";
+})
+document.getElementById("no").addEventListener("click",function(){
+    document.getElementById("display").innerText="😥😥😥";
+})
