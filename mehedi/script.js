6232430a92c8d33b1547d33ff47cd2e66d4cb166
Sabbir hossen <74649145+Sabbir9535@users.noreply.github.com>
2024-10-15 21:38:15 +0600

    Delete mehedi directory

--- mehedi/script.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("yes").addEventListener("click",function(){
-document.getElementById("display").innerText="I love you Fariha";
-})
-document.getElementById("no").addEventListener("click",function(){
-    document.getElementById("display").innerText="😥😥😥";
-})
